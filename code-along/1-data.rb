8bd5dba04bc736be2a012c9f987f589062b3f4c9
Lucie Oken <lucie.oken@kellogg.northwestern.edu>
2024-04-19 14:00:58 +0000

    Class code along completed

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,49 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5
+puts 2
 
-# Non-numbers
+puts 5 + 2
+puts 5 * 2
+puts 5 - 2
+puts 5 / 2.0
+puts (5 - 2) * 3
+
+# Class note - Need to have a float decimal in order to get the remainder for the division line
+
+
+# Non-numbers - always need to do double quotes
+# Use Command / to make a text line
+puts "Hello World!"
+puts "Tacos are" + " delish"
+puts "tacos " * 3
 
 # True, False
+# puts True
+# puts False
+puts 3 == 2
 
 # Nothing
 
 # Variables
+zebra = 5
+giraffe = 2
+
+# puts zebra + giraffe
+food = "tacos"
+quantity = 3
+
+puts food * quantity
 
 # Combine strings and variables
+# puts "I want #{} tacos"
+puts "I want #{quantity} #{food}"
+
+name = "Lucie"
+puts "Hello #{name}!"
+
+# Curly brackets tells computer that before you have this non-numeric value, evaluate some other Ruby code. 
+# We do this so we can make things dynamic, like the greeting above
 
 # String manipulation
