6bc1796cac1db16cfb30d37c735ddfc4644d4584
Lucie Oken <lucie.oken@kellogg.northwestern.edu>
2024-04-19 16:29:20 +0000

    loop array practice in loops lab

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -26,6 +26,13 @@ spades = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
+# can loop the arrays
+for rank in ranks
+    for suit in suits
+    puts "#{rank} of #{suit}"
+    end
+end
+
 # CHALLENGE #2
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
