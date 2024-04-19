6659e3f3fc26065af6afa40dfbd7a77e9d2c50fd
Lucie Oken <lucie.oken@kellogg.northwestern.edu>
2024-04-19 15:24:08 +0000

    Completed arrays and hashes code alongs

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,27 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = { "name" => "Lucie", "location" => "Chicago", "status" => "Learning in ENTR-451!" }
+p profile
+# { :name => "Lucie"}
+# { name: "Lucie"}
+
+# Use [for arrays] and {for hashes}
 
 # Accessing data from the hash
+name = profile["name"]
+p name
+p profile ["status"]
+p profile ["age"]
+
+profile["age"] = 28
+profile["status"] = "Learning in ENTR-451, sec 41!"
+p profile
+
+
 
 # More Complex Hashes
+profile["location"] = { "city" => "Chicago", "state" => "IL"}
+p profile["location"]["city"]
+
+profile["timeline"] = ["woke up", "walked to Hub", "learning"]
